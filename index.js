fab70e5efe58eaf48c1722d9e2291f8b3c71f279
Petersticks1 <124896550+Petersticks1@users.noreply.github.com>
2025-01-24 15:09:56 +0100

    converting-to-web-app

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
-import { registerRootComponent } from 'expo';
+// import { registerRootComponent } from 'expo';
 
+// import App from './App';
+
+// // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
+// // It also ensures that whether you load the app in Expo Go or in a native build,
+// // the environment is set up appropriately
+// registerRootComponent(App);
+
+
+
+import { AppRegistry } from 'react-native';
+import { name as appName } from './app.json';
 import App from './App';
 
-// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// It also ensures that whether you load the app in Expo Go or in a native build,
-// the environment is set up appropriately
-registerRootComponent(App);
+AppRegistry.registerComponent(appName, () => App);
+AppRegistry.runApplication(appName, {
+  initialProps: {},
+  rootTag: document.getElementById('root'),
+});
